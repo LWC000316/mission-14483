f60d53c65d4e7ed268edc3298753469f4dd6bf3a
LWC000316 <lwc000316@naver.com>
2025-09-03 23:36:29 +0900

    t1 t2 t3 t4 blue

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,7 +1,18 @@
 package com.ll;
 
 public class Calc {
-    public static int run(String expr) {
-        return 2;
+    public static int run(String str) {
+        // 공백 제거
+        str = str.replace(" ", "");
+
+        // '+' 연산자로 분리
+        String[] parts = str.split("\\+");
+
+        int result = 0;
+        for (String part : parts) {
+            result += Integer.parseInt(part);
+        }
+        return result;
     }
 }
+
